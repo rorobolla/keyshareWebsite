a47e58a0d4e54140d024277aad68a1d920e7998d
rorobolla <70610965+rorobolla@users.noreply.github.com>
2024-11-14 11:29:55 -0600

    inital commit

---
+++ keyshareWebsite/src/index.php
@@ -0,0 +1,364 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <!-- Meta Tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Title -->
+  <title>PassiveBolt</title>
+
+<?php include( "includes/header.php" ) ?>
+
+  <!-- ========== MAIN CONTENT ========== -->
+  <main id="content">
+    <!-- Hero -->
+    <div class="container content-space-t-3">
+      <div class="row justify-content-lg-between align-items-lg-center">
+        <div class="col-lg-5 mb-5 mb-lg-0">
+          <div class="mb-5">
+            <h1 class="display-4 text-dark mb-5">Pioneering Web3  <br><span class="text-primary">Access Control</span></h1>
+            <p class="fs-3">Decentralized identity platform that introduces cryptographic attestations for unlocking secure spaces.</p>
+          </div>
+
+          <div class="d-grid d-sm-flex gap-3 mb-5">
+            <a class="btn btn-primary" href="/contact/">Partner With Us</a>
+            <a class="btn btn-ghost-dark btn-pointer" href="/article-list/">Read Articles</a>
+          </div>
+        </div>
+        <!-- End Col -->
+
+        <div class="col-lg-6">
+          <div class="position-relative">
+            <div class="position-relative">
+              <img class="img-fluid" src="/assets/img/man-working-laptop.jpg" alt="PassiveBolt Man">
+            </div>
+
+            <!-- Media -->
+            <div class="d-none d-sm-inline-block position-absolute top-0 start-0 bg-white w-100 rounded-pill shadow-sm p-2 mt-5 ms-n5" style="max-width: 12rem;">
+              <div class="d-flex align-items-center">
+                <div class="flex-shrink-0">
+                  <span class="avatar avatar-sm avatar-circle">
+                    <img class="avatar-img" src="/assets/img/160x160/img10.jpg" alt="Avatar">
+                  </span>
+                </div>
+                <div class="flex-grow-1 ms-2">
+                  <div class="fs-5 fw-bold mb-0">Alice</div>
+                  <span class="d-block fs-6">Front door unlocked</span>
+                </div>
+              </div>
+            </div>
+            <!-- End Media -->
+
+            <!-- Media -->
+            <div class="d-none d-sm-inline-block position-absolute bottom-0 start-0 bg-white w-100 rounded-pill shadow-sm p-2 mb-10 ms-n10" style="max-width: 16rem;">
+              <div class="d-flex align-items-center">
+                <div class="flex-shrink-0">
+                  <span class="avatar avatar-sm avatar-circle">
+                    <img class="avatar-img" src="/assets/img/160x160/img3.jpg" alt="Avatar">
+                  </span>
+                </div>
+                <div class="flex-grow-1 ms-2">
+                  <div class="fs-5 fw-bold text-dark mb-0">Bob</div>
+                  <span class="d-block fs-6 text-dark">Mobile key delivered</span>
+                </div>
+              </div>
+            </div>
+            <!-- End Media -->
+          </div>
+        </div>
+        <!-- End Col -->
+      </div>
+      <!-- End Row -->
+    </div>
+    <!-- End Hero -->
+
+    <!-- Icon Blocks -->
+    <div class="container content-space-t-2 content-space-t-lg-3">
+      <div class="row">
+        <div class="col-sm-6 col-lg mb-5 mb-lg-0">
+          <!-- Icon Block -->
+          <div class="text-center box-link">
+            <div class="mb-3">
+              <i class="bi-layers fs-1 text-dark icon"></i>
+            </div>
+            
+            <h5>Web3 Platform</h5>
+            <span class="d-block">Use our identity wallet with cryptographic attestations for unlocking secure spaces.</span>
+            <a href="/web3/" class="stretched-link"></a>
+          </div>
+          <!-- End Icon Block -->
+        </div>
+        <!-- End Col -->
+
+        <div class="col-sm-6 col-lg mb-5 mb-lg-0">
+          <!-- Icon Block -->
+          <div class="text-center box-link">
+            <div class="mb-3">
+              <i class="bi-building fs-1 text-dark icon"></i>
+            </div>
+            
+            <h5>Hotels & Car Rentals</h5>
+            <span class="d-block">Automate check-in process and mobile key delivery for a seamless experience.</span>
+            <a href="/hospitality/" class="stretched-link"></a>
+          </div>
+          <!-- End Icon Block -->
+        </div>
+        <!-- End Col -->
+
+        <div class="col-sm-6 col-lg mb-5 mb-lg-0">
+          <!-- Icon Block -->
+          <div class="text-center">
+            <div class="mb-3">
+              <i class="bi-door-closed fs-1 text-dark icon"></i>
+            </div>
+            
+            <h5>Access Control</h5>
+            <span class="d-block">Control access to secure spaces with cost effective and cutting edge innovations.</span>
+            
+          </div>
+          <!-- End Icon Block -->
+        </div>
+        <!-- End Col -->
+
+        <div class="col-sm-6 col-lg mb-5 mb-lg-0">
+          <!-- Icon Block -->
+          <div class="text-center">
+            <div class="mb-3">
+              <i class="bi-shield-lock fs-1 text-dark icon"></i>
+            </div>
+            
+            <h5>Lock Manufacturers</h5>
+            <span class="d-block">Offering the most advanced smart lock features and mechatronic for a superior user experience.</span>
+            
+          </div>
+          <!-- End Icon Block -->
+        </div>
+        <!-- End Col -->
+      </div>
+      <!-- End Row -->
+    </div>
+    <!-- End Icon Blocks -->
+
+    <!-- Features -->
+    <div class="overflow-hidden">
+      <div class="container content-space-1 content-space-lg-3">
+        <div class="row align-items-lg-center">
+          <div class="col-lg-7 me-auto ms-lg-n10 mb-5 mb-lg-0">
+            <div class="row align-items-center">
+              <div class="col-4">
+                <img class="img-fluid rounded-3" src="/assets/img/woman-smartlock.jpg" alt="Woman Smartlock">
+              </div>
+              <!-- End Col -->
+              
+              <div class="col-3">
+                <img class="img-fluid rounded-3" src="/assets/img/card-swiping-smartlock.jpg" alt="SmartLock">
+              </div>
+              <!-- End Col -->
+
+              <div class="col-5">
+                <img class="img-fluid rounded-3" src="/assets/img/phone-smartlock.jpg" alt="SmartLock">
+              </div>
+              <!-- End Col -->
+            </div>
+            <!-- End Row -->
+          </div>
+          <!-- End Col -->
+
+          <div class="col-lg-5">
+            <div class="mb-5">
+              <h2>Partnering with access control & lock companies</h2>
+              <p>Enabling partners to deploy innovative technologies and solutions as the first-ever Tier1 supplier.</p>
+            </div>
+
+            <!-- List Checked -->
+            <ul class="list-checked list-checked-soft-bg-primary list-checked-lg">
+              <li class="list-checked-item">Keep up with <span class="fw-bold">innovation</span> cycles</li>
+              <li class="list-checked-item">Significantly reduce <span class="fw-bold">costs</span></li>
+              <li class="list-checked-item"><span class="fw-bold">Faster</span> time to market</li>
+            </ul>
+            <!-- End List Checked -->
+          </div>
+          <!-- End Col -->
+        </div>
+        <!-- End Row -->
+      </div>
+    </div>
+    <!-- End Features -->
+
+    <!-- Icon Blocks -->
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-6 col-lg-4 mb-5">
+          <div class="text-center px-md-5">
+            <div class="mb-3">
+              <i class="bi-grid fs-1 text-dark"></i>
+            </div>
+            <h5 class="text-primary">Decentralized Identity</h5>
+            <p>A blockchain registry for universally unique identifiers to enable access control using Web3 identity wallets.</p>
+          </div>
+        </div>
+        <!-- End Col -->
+
+        <div class="col-sm-6 col-lg-4 mb-5">
+          <div class="text-center px-md-5">
+            <div class="mb-3">
+              <i class="bi-card-heading fs-1 text-dark"></i>
+            </div>
+            <h5 class="text-primary">Cryptographic Attestations</h5>
+            <p>A way to securely authorize access for an individual as opposed to an object (eg. cards) or a secret (eg. pin code).</p>
+          </div>
+        </div>
+        <!-- End Col -->
+
+        <div class="col-sm-6 col-lg-4 mb-5">
+          <div class="text-center px-md-5">
+            <div class="mb-3">
+              <i class="bi-person-check fs-1 text-dark"></i>
+            </div>
+            <h5 class="text-primary">Readers & Control Panels</h5>
+            <p>Most advanced reader technologies that support cryptographic and traditional credentials.</p>
+          </div>
+        </div>
+        <!-- End Col -->
+
+        <div class="col-sm-6 col-lg-4 mb-5 mb-lg-0">
+          <div class="text-center px-md-5">
+            <div class="mb-3">
+              <i class="bi-broadcast fs-1 text-dark"></i>
+            </div>
+            <h5 class="text-primary">Connectivity</h5>
+            <p>Modular designs for almost all protocols to provide connectivity to lock and access control systems.</p>
+          </div>
+        </div>
+        <!-- End Col -->
+
+        <div class="col-sm-6 col-lg-4 mb-5 mb-sm-0">
+          <div class="text-center px-md-5">
+            <div class="mb-3">
+              <i class="bi-gear fs-1 text-dark"></i>
+            </div>
+            <h5 class="text-primary">Mechatronic</h5>
+            <p>The first gearless direct actuation technology for locks that is highly energy efficient, quiet, and compact.</p>
+          </div>
+        </div>
+        <!-- End Col -->
+
+        <div class="col-sm-6 col-lg-4">
+          <div class="text-center px-md-5">
+            <div class="mb-3">
+              <i class="bi-cpu fs-1 text-dark"></i>
+            </div>
+            <h5 class="text-primary">Sensors</h5>
+            <p>Proprietary algorithms and designs including door tracking, micro location, touch detection, and more.</p>
+          </div>
+        </div>
+        <!-- End Col -->
+      </div>
+      <!-- End Row -->
+    </div>
+    <!-- End Icon Blocks -->
+
+    <!-- Features -->
+    <div class="container content-space-t-2 content-space-t-lg-3">
+      <div class="row justify-content-lg-between align-items-lg-center">       
+        <div class="col-lg-6 mb-5 mb-lg-0">
+            <img class="img-fluid rounded-3" src="/assets/img/quality-control-concept.jpg" alt="Quality Control"> 
+        </div>
+        <!-- End Col -->
+
+        <div class="col-lg-5">
+          <div class="mb-5">
+            <h2>We are shaping the future</h2>
+            <p>Web3 and decentralized identity have unlocked an entirely new class of access control and security solutions based on cryptographic credentialing. PassiveBolt has pioneered enabling technologies for industry partners to launch solutions faster and more cost effectively.</p>
+          </div>
+
+          <!-- List Checked -->
+          <ul class="list-checked list-checked-soft-bg-primary list-checked-lg mb-5">
+            <li class="list-checked-item"><span class="fw-bold">Hardware</span> (web3 capable locks, readers, and panels)</li>
+            <li class="list-checked-item"><span class="fw-bold">Decentralized Apps</span> (dApps)</li>
+            <li class="list-checked-item"><span class="fw-bold">Decentralized Web App</span> (DWA)</li>
+          </ul>
+          <!-- End List Checked -->
+
+          <a class="btn btn-primary" href="/web3/">Learn More</a>
+        </div>
+        <!-- End Col -->
+      </div>
+      <!-- End Row -->
+    </div>
+    <!-- End Features -->
+
+    <!-- Stats -->
+    <div class="container content-space-t-1 content-space-b-2 content-space-b-sm-3 content-space-lg-2 content-space-lg-3">
+      <div class="row justify-content-lg-between align-items-lg-center">
+        <div class="col-lg-5 mb-9 mb-lg-0 pe-lg-5">
+          <div class="mb-6">
+            <h2>Why the Tier1 model?</h2>
+            <p>Mature industries leverage a Tier1 supply structure in order to realize a more cost-effective R&D, adapt to new innovation cycles faster, leverage Tier1 knowledge base to implement best design practices, and take the pain out of interoperability.</p>
+          </div>
+
+          <!-- Blockquote -->
+          <figure>
+            <blockquote class="blockquote"><em>We use Passivebolt as our Tier1. They are a force multiplier and provide us with the fastest and most cost effective path to market with our lock solutions.</em></blockquote>
+
+            <figcaption class="blockquote-footer">
+              <div class="d-flex align-items-center">
+
+                <div class="flex-grow-1 ms-3">
+                Zachary Klares
+                  <span class="blockquote-footer-source">VP of Electronics | PDQ Locks</span>
+                </div>
+              </div>
+            </figcaption>
+          </figure>
+          <!-- End Blockquote -->
+        </div>
+        <!-- End Col -->
+
+        <div class="col-lg-7 ps-lg-6">
+          <img class="img-fluid shadow-1" src="/assets/img/tier-1-supply-chain-structure-2.png" alt="Tier 1 Supply Chain Structure">
+        </div>
+        <!-- End Col -->
+      </div>
+      <!-- End Row -->
+    </div>
+    <!-- End Stats -->
+
+    <!-- Clients -->
+    <div class="container">
+      <div class="row logo-row">
+        <div class="col-lg-2 col-md-4 col-sm-6 mb-4 d-flex justify-content-center align-items-center">
+          <img class="logo-3" src="/assets/img/CES2020.png" alt="CES2020 Logo">
+        </div>
+
+        <div class="col-lg-2 col-md-4 col-sm-6 mb-4 d-flex justify-content-center align-items-center">
+          <img class="logo-3" src="/assets/img/CES2021.png" alt="CES2021 Logo">
+        </div>
+
+        <div class="col-lg-2 col-md-4 col-sm-6 mb-5 mb-sm-4 mb-4 d-flex justify-content-center align-items-center">
+          <img class="logo-2" src="/assets/img/iot-awards-logo.png" alt="IOT Awards Logo">
+        </div>
+
+        <div class="col-lg-2 col-md-4 col-sm-6 mb-4 d-flex justify-content-center align-items-center">
+          <img class="logo-3" src="/assets/svg/brands/forbes-dark.svg" alt="Forbes Logo">
+        </div>
+
+        <div class="col-lg-2 col-md-4 col-sm-6 mb-4 d-flex justify-content-center align-items-center">
+          <img class="logo-1" src="/assets/img/cnet-logo-4.png" alt="cnet Logo">
+        </div>
+
+        <div class="col-lg-2 col-md-4 col-sm-6 mb-sm-4 d-flex justify-content-center align-items-center">
+          <img class="logo-2" src="/assets/img/techhive.png" alt="TechHive Logo">
+        </div>
+
+      </div>
+    </div>
+    <!-- End Clients -->
+
+<?php include( "includes/related-articles.php" ) ?>
+
+  </main>
+  <!-- ========== END MAIN CONTENT ========== -->
+
+<?php include( "includes/footer.php" ) ?>

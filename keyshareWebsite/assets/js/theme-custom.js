9c793c5ebd9094b409ebdf72775598f6a1afd59b
rorobolla <70610965+rorobolla@users.noreply.github.com>
2024-12-01 11:54:58 -0600

    Add files via upload
    final one page first iteration

--- keyshareWebsite/assets/js/theme-custom.js
+++ keyshareWebsite/assets/js/theme-custom.js
@@ -1,1 +1,29 @@
 // File for your custom JavaScript
+// Get the container element
+var btnContainer = document.getElementById("featuresTab");
+
+// Get all buttons with class="btn" inside the container
+var btns = btnContainer.getElementsByClassName("nav-link");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
+var btnContainer = document.getElementById("imagesTab");
+
+// Get all buttons with class="btn" inside the container
+var btns = btnContainer.getElementsByClassName("img");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
